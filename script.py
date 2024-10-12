1e80395f13e32637a752babc2b435cf315e9015c
KirGorbunov <KirGorbunov@gmail.com>
2024-10-12 12:09:18 +0300

    Add synchronous init_db function for database initialization

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+import sqlalchemy.exc
 from sqlalchemy import Column, Float, Integer, String, DateTime
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
 
 # Настройки базы данных PostgreSQL
 DATABASE_URL = "postgresql+asyncpg://weather_user:weather_password@localhost/weather_db"
@@ -9,6 +10,7 @@ SYNC_DATABASE_URL = "postgresql://weather_user:weather_password@localhost/weathe
 
 # Создание базы данных с помощью SQLAlchemy
 Base = declarative_base()
+
 
 # Модель данных для хранения погоды
 class Weather(Base):
@@ -27,8 +29,23 @@ class Weather(Base):
     showers = Column(Float, nullable=True)
     snowfall = Column(Float, nullable=True)
 
-# Асинхронный движок для подключения к базе
-engine = create_async_engine(DATABASE_URL)
+
+# Движки для подключения к базе
+async_engine = create_async_engine(DATABASE_URL)
+sync_engine = create_engine(SYNC_DATABASE_URL)
+
 async_session = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
+    async_engine, class_=AsyncSession, expire_on_commit=False
 )
+
+
+# Синхронный движок для создания таблиц
+def init_db():
+    try:
+        with sync_engine.begin() as conn:
+            Base.metadata.create_all(conn)
+        print("Таблицы успешно созданы или уже существуют.")
+    except sqlalchemy.exc.OperationalError as e:
+        print(f"Ошибка доступа к базе данных: {e}")
+    except sqlalchemy.exc.SQLAlchemyError as e:
+        print(f"Ошибка работы с базой данных: {e}")
